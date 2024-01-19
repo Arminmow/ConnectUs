986aed697d9ba7695b52ddba2a153dd715005c94
Armin <arminmow@gmail.com>
2024-01-19 19:34:04 +0330

    Showing status replies

--- resources/views/timeLine/index.blade.php
+++ resources/views/timeLine/index.blade.php
@@ -43,9 +43,47 @@
                                 <small class="text-muted">{{ $status->created_at->diffForHumans() }}</small>
 
                                 <!-- Like Button and Number of Likes -->
-                                <div class="mt-3">
+                                <div class="mt-3" >
                                     <button type="button" class="btn btn-primary btn-sm">Like</button>
                                     <span class="ml-2">10 Likes</span>
+                                    <!-- Replies -->
+                                    @foreach($status->replies as $reply)
+
+                                        <div class="card-body">
+                                            <!-- User Profile Image -->
+                                            <a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">
+                                                <img src="{{ $reply->user->getAvatarUrl() }}" alt="{{ $reply->user->getNameOrUsername() }}" class="img-fluid rounded-circle" style="width: 50px; height: 50px;">
+                                            </a>
+                                            <!-- User Name -->
+                                            <a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">
+                                                <h5 class="card-title ml-2">{{ $reply->user->getNameOrUsername() }}</h5>
+                                            </a>
+                                            <!-- User Comment -->
+                                            <p class="card-text mt-2">{{ $reply->body }}</p>
+
+                                            <!-- Time Posted -->
+                                            <small class="text-muted">{{ $reply->created_at->diffForHumans() }}</small>
+
+                                            <!-- Like Button and Number of Likes -->
+                                            <div class="mt-3" style="display: inline-block">
+                                                <button type="button" class="btn btn-primary btn-sm">Like</button>
+                                                <span class="ml-2">10 Likes</span>
+
+{{--                                                <!-- Reply Form -->--}}
+{{--                                                <form role="form" action="{{ route('status.reply' , ['statusId' =>$status->id ]) }}" method="post" class="mt-3">--}}
+{{--                                                    <div class="form-group">--}}
+{{--                                                        <textarea class="form-control{{ $errors->has("reply-". $status->id) ? ' is-invalid' : '' }}" name="reply-{{ $status->id }}" placeholder="Reply to this status"></textarea>--}}
+{{--                                                        <span class="help-block invalid-feedback">{{ $errors->first("reply-". $status->id) }}</span>--}}
+{{--                                                    </div>--}}
+{{--                                                    <input type="submit" class="btn btn-success mt-2" value="Reply">--}}
+{{--                                                    <input type="hidden" name="_token" value="{{ Session::token() }}">--}}
+{{--                                                </form>--}}
+                                            </div>
+                                        </div>
+
+                                    @endforeach
+
+
 
                                     <!-- Reply Form -->
                                     <form role="form" action="{{ route('status.reply' , ['statusId' =>$status->id ]) }}" method="post" class="mt-3">
